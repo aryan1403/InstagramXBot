f47e2e3590c6dbd7b52fb2018be5398f1468e29a
Aaryan <aaryan14032006@gmail.com>
2021-08-08 12:55:22 +0530

    Updated

--- src/main/java/com/hellionbots/Helpers/credHelper.java
+++ src/main/java/com/hellionbots/Helpers/credHelper.java
@@ -5,14 +5,14 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 public class credHelper {
     // Check's if the username is present
     public boolean isUPresent(Update update){
-        if(new credentials(update).getUsername(update) == null) 
+        if(new credentials(update).username == null)
             return false;
         return true;
     }
 
     // Check's if the password is present
     public boolean isPPresent(Update update){
-        if(new credentials(update).getPass(update) != null) 
+        if(new credentials(update).password == null) 
             return false;
         return true;
     }
