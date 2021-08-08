9c3c0ffbc62242e2c45ad4b827711fccf854c586
Aaryan <aaryan14032006@gmail.com>
2021-08-08 11:54:25 +0530

    Connected DB with cloud

--- src/main/java/com/hellionbots/DB/db.java
+++ src/main/java/com/hellionbots/DB/db.java
@@ -1,5 +1,8 @@
 package com.hellionbots.DB;
 
+import com.hellionbots.Helpers.configuration;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
@@ -8,28 +11,34 @@ import com.mongodb.client.model.Filters;
 import org.bson.Document;
 
 public class db {
-    private final String MongoUrl = "mongodb://localhost:27017";
+    private final String connection = configuration.conn;
     private final String dbName = "test";
     private final String collName = "testing";
     
     public boolean add(Document document){
         try {
-            MongoClient client = MongoClients.create(MongoUrl);
+            ConnectionString connectionString = new ConnectionString(connection);
+            MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+            MongoClient client = MongoClients.create(settings);
             MongoDatabase database = client.getDatabase(dbName);
             MongoCollection<Document> doc = database.getCollection(collName);
 
             doc.insertOne(document);
             return true;
         } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("ERROR Occured Inserting Data..");
             return false;
         }
     }
 
     public void alreadyPresent(String id){
         try {
-            MongoClient client = MongoClients.create(MongoUrl);
+            ConnectionString connectionString = new ConnectionString(connection);
+            MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+            MongoClient client = MongoClients.create(settings);
             MongoDatabase database = client.getDatabase(dbName);
             MongoCollection<Document> doc = database.getCollection(collName);
 
@@ -39,14 +48,16 @@ public class db {
                 doc.deleteOne(d);
             }
         } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("ERROR Occured Deleting Data..");
         }
     }
 
     public String findUsername(String id){
         try {
-            MongoClient client = MongoClients.create(MongoUrl);
+            ConnectionString connectionString = new ConnectionString(connection);
+            MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+            MongoClient client = MongoClients.create(settings);
             MongoDatabase database = client.getDatabase(dbName);
             MongoCollection<Document> doc = database.getCollection(collName);
 
@@ -54,15 +65,17 @@ public class db {
             String username = d.get("username").toString();
             return username;
         } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("ERROR Occured Finding Username..");
             return null;
         }
     }
 
     public String findPassword(String id){
         try {
-            MongoClient client = MongoClients.create(MongoUrl);
+            ConnectionString connectionString = new ConnectionString(connection);
+            MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+            MongoClient client = MongoClients.create(settings);
             MongoDatabase database = client.getDatabase(dbName);
             MongoCollection<Document> doc = database.getCollection(collName);
 
@@ -70,21 +83,22 @@ public class db {
             String password = d.get("password").toString();
             return password;
         } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("ERROR Occured Finding Password..");
             return null;
         }
     }
 
     public Document get(String id){
         try {
-            MongoClient client = MongoClients.create(MongoUrl);
+            ConnectionString connectionString = new ConnectionString(connection);
+            MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+            MongoClient client = MongoClients.create(settings);
             MongoDatabase database = client.getDatabase(dbName);
             MongoCollection<Document> doc = database.getCollection(collName);
 
             return doc.find(Filters.eq("key", id)).first();
         } catch (Exception e) {
-            System.out.println("ERROR Occured Retreiving Data..");
             return null;
         }
     }
