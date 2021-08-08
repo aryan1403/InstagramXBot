4f9e2ca6febf55bbddad8f6551b8fa8a21424b20
Aaryan <aaryan14032006@gmail.com>
2021-08-08 07:23:50 +0530

    Created set cred plugin

---
+++ src/main/java/com/hellionbots/Plugins/setCredentials.java
@@ -0,0 +1,16 @@
+package com.hellionbots.Plugins;
+
+import com.hellionbots.InstaX;
+import com.hellionbots.Master;
+
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class setCredentials extends InstaX implements Master {
+
+    @Override
+    public void handleRequests(Update update, String cmd) {
+        
+        
+    }
+    
+}
