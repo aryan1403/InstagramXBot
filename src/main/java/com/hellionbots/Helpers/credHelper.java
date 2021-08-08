2d4e640c8cadc9ae687f0544684daf25fbdafc3d
Aaryan <aaryan14032006@gmail.com>
2021-08-08 07:23:24 +0530

    Credentails Helper

---
+++ src/main/java/com/hellionbots/Helpers/credHelper.java
@@ -0,0 +1,17 @@
+package com.hellionbots.Helpers;
+
+public class credHelper {
+    // Check's if the username is present
+    public boolean isUPresent(){
+        if(credentials.username == null) 
+            return false;
+        return true;
+    }
+
+    // Check's if the password is present
+    public boolean isPPresent(){
+        if(credentials.username == null) 
+            return false;
+        return true;
+    }
+}
