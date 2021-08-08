4fc33932c09d05dfe64a253477275b5a3d8827f6
Aaryan <aaryan14032006@gmail.com>
2021-08-08 10:24:43 +0530

    Created Help Plugin

---
+++ src/main/java/com/hellionbots/Plugins/help.java
@@ -0,0 +1,23 @@
+package com.hellionbots.Plugins;
+
+import com.hellionbots.InstaX;
+import com.hellionbots.Master;
+
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class help extends InstaX implements Master{
+
+    @Override
+    public void handleRequests(Update update, String cmd) {
+        if(cmd.equalsIgnoreCase(getHandler()+"help")){
+            sendMessage(update, "Welcome to the Help Section of InstaX\n"
+            + "Available Commands :-\n"
+            + "1. /setcred - Enter your Credential's for Login\n"
+            + "2. /post - Reply to a photo to Post it.\n"
+            + "For Any Feedback or Help Join "+support()
+            + "\n\nPowered by "+channel());
+        }
+        
+    }
+    
+}
