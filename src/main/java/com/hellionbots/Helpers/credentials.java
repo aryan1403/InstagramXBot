f5070e372c4ac8c5db4c88e9bd3280fa6138c6d8
Aaryan <aaryan14032006@gmail.com>
2021-08-08 07:23:14 +0530

    Created credentails file

---
+++ src/main/java/com/hellionbots/Helpers/credentials.java
@@ -0,0 +1,14 @@
+package com.hellionbots.Helpers;
+
+public class credentials {
+    public static String username;
+    public static String password;
+
+    public void setUsername(String username){
+        credentials.username = username;
+    }
+
+    public void setPass(String password) {
+        credentials.password = password;
+    }
+}
